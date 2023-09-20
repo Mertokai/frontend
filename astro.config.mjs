c4dc601643d14519fd35826a42e2e77733b0206b
Mertokai <merteyuboglu.16@hotmail.com>
2023-09-21 01:27:58 +0300

    Endüstri ve liderlik topluluğu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: "sqhma3n4",
+      dataset: "production",
+      apiVersion: "2023-02-08",
+      useCdn: false,
+    }),
+    react(),
+  ],
+});
